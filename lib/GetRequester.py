04cf1a0c4ab4723073ee1559929ab8b66cfb0bc2
Nobert-Muma <mumanobert17@gmail.com>
2024-10-06 14:26:15 +0300

    Getting Remote Data Lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -4,10 +4,16 @@ import json
 class GetRequester:
 
     def __init__(self, url):
-        self.url = url
+        self.url=url
 
     def get_response_body(self):
-        pass
-
+        response=requests.get(self.url)
+        return response.content
+        
     def load_json(self):
-        pass
+        result=self.get_response_body()
+        return json.loads(result)
+
+
+result=GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json').load_json()
+print(result)
